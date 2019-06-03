beba415732b8440de015ce444c8f6d2784836d8c
mxdi9i7 <peter.zheng88228@gmail.com>
2019-06-03 19:51:16 -0400

    initial commit

---
+++ routes/posts.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.json({
+    success: true
+  })
+});
+
+router.get('/category', (req, res) => {
+  res.json({
+    success:true
+  })
+})
+
+module.exports = router;
